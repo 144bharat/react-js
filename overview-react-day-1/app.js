889656dd2e4bdc0c86470fcb4e479feeb97ceb03
144bharat <144bharatgautam@gmail.com>
2026-03-25 02:45:01 +0530

    doc: Day - 2 Making our app production ready from scratch using parcel and react dependencies.

--- overview-react-day-1/app.js
+++ overview-react-day-1/app.js
@@ -6,6 +6,10 @@
 
 // 14-03-2026 - Day-2: Now i will create the same h1 tag with content using react js
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));        
 const h1 = React.createElement(
     "h1",
