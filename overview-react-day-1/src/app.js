b5b7e30c71ae643bc48db466e816143280cbeb5f
144bharat <144bharatgautam@gmail.com>
2026-04-28 16:04:22 +0530

    feat: 1. Add filter button for 4 star above rated restro using useState. 2. Tried to add search feature with useState.

--- overview-react-day-1/src/app.js
+++ overview-react-day-1/src/app.js
@@ -7,35 +7,6 @@ import Body from "./components/Body";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));        
 
-/*
-//THIS IS MY REACT ELEMENT
-const h1 = React.createElement(
-    "h1",
-    {id:"h1tag"},
-    "I am the text inside h1 bharat"
-);
-
-//THIS IS JSX
-const jsxh1 = (<h1>i am text inside jsx updated</h1>);
-
-
-//JSX (this is not html but kind of same in look) --> parcel (Pass JSX to Babel) --> Babel --> React Element changed (by babel) --> rendered on root.
-
-// A Component --> Function --> Returns --> JSX.
-
-const HeadComponent = () =>{
-    return (
-        <div>
-            {//COMPONENT INSIDE COMPONENT IS CALLED "COMPONENT COMPOSITION"}
-            <TitleComponent/>
-            <h1>This heading is special and coming from header component.</h1>
-        </div>
-    )
-};
-
-const TitleComponent = () => (<h1>This is my Title</h1>);
-*/
-
 const Footer = () => {
     return (
         <div className="footer"></div>
--- overview-react-day-1/src/components/Body.js
+++ overview-react-day-1/src/components/Body.js
@@ -1,20 +1,44 @@
 import RestroCard from "./RestroCard";
 import { Search, Filter } from "lucide-react";
 import resDataList from "../utils/mockData";
+import { useState } from "react";
+
+
+
 
 const Body = () => {
+    
+let [restroList,setRestroList]=useState(resDataList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    
+function filter4StarAboveRestros() {
+    let filteredTopRatedRestroList = restroList.filter(restro => restro.info.avgRating>4.5);
+    console.log(filteredTopRatedRestroList);
+    setRestroList(filteredTopRatedRestroList); //This special utility function get my new filtered data to hook with the current data we are displaying on frontend.
+}
+
+let [searchBarText, setSearchBarText]=useState("");
+
+const updateSearchBarText = (event) => {
+    setSearchBarText(event.target.value.toLowerCase());
+    console.log("[A]: "+searchBarText);
+
+    let searchedRestroList = resDataList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants.filter(restroObj => restroObj.info.name.includes(searchBarText));
+    setRestroList(searchedRestroList);
+}
+
     return (
         <main className="main">
             <div className="searchContainer">
-                <input type="search" name="search" placeholder="Search food items..."/>
+                <input type="search" name="search" placeholder="Search food items..." onChange={updateSearchBarText}/>
                 <button type="button" className="searchbtn"><Search/></button>
-                <button type="button" className="topratedfilterbtn" onClick={()=>{console.log("On Click in Action.")}}>Top Rated Restraurants<Filter/> </button>
+                <button type="button" className="topratedfilterbtn" onClick={filter4StarAboveRestros}>Top Rated Restraurants<Filter/> </button>
             </div>
             <div className="restroContainer">
                 {/* <RestroCard resData={resDataList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants[0].info}/> */}
 
                 {
-                    resDataList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants.map(restro => <RestroCard key={restro.info.id} resData = {restro.info} />)
+                    //resDataList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants.map(restro => <RestroCard key={restro.info.id} resData = {restro.info} />)
+                    restroList.map(restro => <RestroCard key={restro.info.id} resData = {restro.info} />)
                 }
             </div>
         </main>
