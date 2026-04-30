3fc4e72638b3c3053f0420dabef258fa2fc61660
144bharat <144bharatgautam@gmail.com>
2026-04-30 11:04:10 +0530

    func: 1. search bar fixed. 2. add shimmer ui 3. implemented useEffect for fetching data from api.

--- overview-react-day-1/src/components/Body.js
+++ overview-react-day-1/src/components/Body.js
@@ -1,36 +1,57 @@
 import RestroCard from "./RestroCard";
 import { Search, Filter } from "lucide-react";
-import resDataList from "../utils/mockData";
-import { useState } from "react";
-
+//import resDataList from "../utils/mockData";
+import { useState, useEffect} from "react";
+import ShimmerCards from "./ShimmerCards";
 
 
 
 const Body = () => {
     
-let [restroList,setRestroList]=useState(resDataList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
-    
+let [restroList,setRestroList]=useState([]); //resDataList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+
+let [restroFilteredList,setRestroFilteredList]=useState([]);
+
+
+//useEffect = > takes 2 argument: callback(), depArray.
+useEffect(()=>{
+fetchRestroData();
+},[]);
+
+let fetchRestroData = async () => {
+    let dataStream = await fetch("https://namastedev.com/api/v1/listRestaurants");
+    let restroListDataFromApi = await dataStream.json();
+    console.log(restroListDataFromApi);
+    setRestroList(restroListDataFromApi?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    setRestroFilteredList(restroListDataFromApi?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+}
+
 function filter4StarAboveRestros() {
     let filteredTopRatedRestroList = restroList.filter(restro => restro.info.avgRating>4.5);
     console.log(filteredTopRatedRestroList);
-    setRestroList(filteredTopRatedRestroList); //This special utility function get my new filtered data to hook with the current data we are displaying on frontend.
+    setRestroFilteredList(filteredTopRatedRestroList); //This special utility function get my new filtered data to hook with the current data we are displaying on frontend.
 }
 
 let [searchBarText, setSearchBarText]=useState("");
 
 const updateSearchBarText = (event) => {
     setSearchBarText(event.target.value.toLowerCase());
-    console.log("[A]: "+searchBarText);
-
-    let searchedRestroList = resDataList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants.filter(restroObj => restroObj.info.name.includes(searchBarText));
-    setRestroList(searchedRestroList);
+    //console.log("[A]: "+searchBarText);
 }
 
+    //Conditional rendering
+    if(restroFilteredList.length === 0){
+        return <ShimmerCards/>;
+    }
     return (
         <main className="main">
             <div className="searchContainer">
-                <input type="search" name="search" placeholder="Search food items..." onChange={updateSearchBarText}/>
-                <button type="button" className="searchbtn"><Search/></button>
+                <input type="text" name="search" placeholder="Search food items..." value={searchBarText} onChange={updateSearchBarText}/>
+                <button type="button" className="searchbtn" onClick={()=>{
+                    let searchedRestroList = restroList.filter(restroObj => restroObj.info.name.toLowerCase().includes(searchBarText.toLowerCase())); //resDataList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+                    setRestroFilteredList(searchedRestroList);
+                }}><Search/></button>
                 <button type="button" className="topratedfilterbtn" onClick={filter4StarAboveRestros}>Top Rated Restraurants<Filter/> </button>
             </div>
             <div className="restroContainer">
@@ -38,7 +59,7 @@ const updateSearchBarText = (event) => {
 
                 {
                     //resDataList.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants.map(restro => <RestroCard key={restro.info.id} resData = {restro.info} />)
-                    restroList.map(restro => <RestroCard key={restro.info.id} resData = {restro.info} />)
+                    restroFilteredList.map(restro => <RestroCard key={restro.info.id} resData = {restro.info} />)
                 }
             </div>
         </main>
---
+++ overview-react-day-1/src/components/ShimmerCards.js
@@ -0,0 +1,29 @@
+const ShimmerCard = () =>{
+    return(
+        <div className="shimmerCard">
+            <div className="img"></div>
+            <div className="txt"></div>
+            <div className="txt"></div>
+            <div className="txt"></div>
+            <div className="btn"></div>
+            <div className="btn"></div>
+        </div>
+    )
+}
+const ShimmerCards = () => {
+    return (
+        <div className="shimmerContainer">
+            <ShimmerCard />
+            <ShimmerCard />
+            <ShimmerCard />
+            <ShimmerCard />
+            <ShimmerCard />
+            <ShimmerCard />
+            <ShimmerCard />
+            <ShimmerCard />
+            <ShimmerCard />
+        </div>
+    )
+}
+
+export default ShimmerCards;
